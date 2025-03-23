ad9d30c4301031d6139b55aaa8f79328438c34fb
Valentin <47915711+alkoria@users.noreply.github.com>
2025-03-23 21:58:33 +0300

    Update ThemeToggler.tsx

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -1,26 +1,59 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
-import { useColorScheme, useToggleColorScheme } from "@/hooks/useColorScheme.web";
-
+import { TouchableOpacity, Text, StyleSheet, Platform } from "react-native";
+import {
+  useColorScheme,
+  useToggleColorScheme,
+} from "@/hooks/useColorScheme.web";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import * as SplashScreen from "expo-splash-screen";
+import { Ionicons } from "@expo/vector-icons";
 export default function ThemeToggler() {
   const scheme = useColorScheme(); // Текущая тема
   const toggleColorScheme = useToggleColorScheme(); // Функция для переключения темы
 
   return (
-    <TouchableOpacity onPress={toggleColorScheme} style={styles.button}>
-      <Text style={styles.text}>
+    <TouchableOpacity
+      onPress={toggleColorScheme}
+      style={[
+        styles.button,
+        { backgroundColor: scheme === "dark" ? "#151718" : "#fff" },
+        { cursor: "pointer" },
+        { alignItems: "center" },
+        {justifyContent: "center"}
+      ]}
+    >
+      {/* <Text style={styles.text}>
         {scheme === "dark" ? "Светлая тема" : "Тёмная тема"}
-      </Text>
+      </Text> */}
+
+      {/* <MaterialCommunityIcons name="theme-light-dark" size={24} color="black" /> */}
+
+      <Ionicons
+        name={scheme === "dark" ? "sunny-outline" : "moon-outline"}
+        size={Platform.OS === "ios" ? 22 : 25}
+        color={scheme === "dark" ? "gold" : "blue"}
+      />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    padding: 8,
-    borderRadius: 4,
-    backgroundColor: "gray",
-    marginVertical: 16,
+    // padding: 8,
+    // borderRadius: 10,
+    // backgroundColor: "ivory",
+    // marginVertical: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
   },
   text: {
     color: "white",
