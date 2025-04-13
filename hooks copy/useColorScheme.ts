8692e1b6a6423e88d80b5893d12a72bfeed415aa
Valentin <47915711+alkoria@users.noreply.github.com>
2025-04-14 00:15:21 +0300

    Create useColorScheme.ts

---
+++ hooks copy/useColorScheme.ts
@@ -0,0 +1,8 @@
+// import { useColorScheme } from "react-native";
+
+export { useColorScheme } from "react-native";
+ 
+// export function useColorScheme(): 'dark' {
+//   return 'dark'; // Всегда возвращает 'dark'
+// }
+
